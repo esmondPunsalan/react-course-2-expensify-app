3eb0051ea479ec4a9c370669b9ff718a2d72d4a0
Esmond Punsalan <esmond.punsalan@neovation.local>
2018-06-28 12:43:57 -0500

    updates

--- src/app.js
+++ src/app.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-
 import AppRouter from './routers/app-router';
 import configureStore from './store/configure-store';
-
 import { addExpense } from './actions/expenses';
 import { sortByDate } from './actions/filters';
 import getVisibleExpenses from './selectors/expenses';
-
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
