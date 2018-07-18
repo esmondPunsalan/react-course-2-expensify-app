4c93a1d447a82f15a1dd69b37d16a523f5a1d178
Esmond Punsalan <esmond.punsalan@neovation.local>
2018-07-18 15:55:32 -0500

    added styling to add expense and edit expense

--- src/components/expenses-summary.js
+++ src/components/expenses-summary.js
@@ -3,14 +3,20 @@ import numeral from "numeral"
 import getExpensesTotal from "../selectors/expenses-total";
 import selectExpenses from '../selectors/expenses';
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 
 export const ExpensesSummary = ({ expensesCount, expensesTotal }) => {
     const expenseWord = expensesCount == 1 ? "expense" : "expenses";
     const formatedTotal = numeral(expensesTotal / 100).format("$0,0.00");
 
     return (
-        <div>
-            Viewing {expensesCount} {expenseWord} totalling {formatedTotal}
+        <div className="page-header">
+            <div className="content-container">
+                <h1 className="page-header__title">Viewing <span>{expensesCount}</span> {expenseWord} totalling <span>{formatedTotal}</span></h1>
+                <div className="page-header__actions">
+                    <Link className="button" to="/create" >+ Expense</Link>
+                </div>
+            </div>
         </div>
     )
 };
